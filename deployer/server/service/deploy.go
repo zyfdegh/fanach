aaadd7547933a9bc82864eecca541b9aa8f1866b
zyfdegh <zyfdegg@gmail.com>
2016-11-27 23:00:33 +0800

    install, enable, start ss on remote server

--- deployer/server/service/deploy.go
+++ deployer/server/service/deploy.go
@@ -26,6 +26,9 @@ func Deploy(reqDeploy *entity.ReqDeploy) (resp entity.RespPostDeploy, err error)
 	c.Port = reqDeploy.Port
 
 	leaveFootprints(c)
+	installDocker(c)
+	enableDocker(c)
+	startSsContainer(c)
 
 	return
 }
@@ -41,16 +44,36 @@ func leaveFootprints(c *SSHConfig) {
 	fmt.Println(output)
 }
 
-func startSsContainer() {
-
-}
-
-func installDocker() {
-	// cmd := "curl https://get.docker.com/ | sh"
-	// executeCmd(config, host, port, cmd)
+func installDocker(c *SSHConfig) {
+	log.Println("installing docker on server...")
+	cmd := "curl https://get.docker.com/ | sh"
+	output, err := executeRemote(c, cmd)
+	if err != nil {
+		log.Printf("execute cmd %s on server error: %v", cmd, err)
+		return
+	}
+	fmt.Println(output)
 }
 
 // startup docker on system boot and launch docker now
-func enableDocker() {
+func enableDocker(c *SSHConfig) {
+	log.Println("enabling docker on server...")
+	cmd := "systemctl enable docker && systemctl start docker"
+	output, err := executeRemote(c, cmd)
+	if err != nil {
+		log.Printf("execute cmd %s on server error: %v", cmd, err)
+		return
+	}
+	fmt.Println(output)
+}
 
+func startSsContainer(c *SSHConfig) {
+	log.Println("starting a ss container on server...")
+	cmd := "docker run -d -p 8387:8388 zyfdedh/shadowsocks:latest -s 0.0.0.0 -p 8388 -k deployer123 -m aes-256-cfb"
+	output, err := executeRemote(c, cmd)
+	if err != nil {
+		log.Printf("execute cmd %s on server error: %v", cmd, err)
+		return
+	}
+	fmt.Println(output)
 }
