c1d16dae5c3ce11439e3d81599ccfeac64dde66d
zyfdegh <zyfdegg@gmail.com>
2017-02-27 13:24:55 +0800

    add sha256

--- coreserver/util/hash.go
+++ coreserver/util/hash.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"crypto/md5"
+	"crypto/sha256"
 	"fmt"
 	"io"
 )
@@ -9,6 +10,13 @@ import (
 // MD5sum hash
 func MD5sum(content string) string {
 	h := md5.New()
+	io.WriteString(h, content)
+	return fmt.Sprintf("%x", h.Sum(nil))
+}
+
+// SHA256 hash
+func SHA256(content string) string {
+	h := sha256.New()
 	io.WriteString(h, content)
 	return fmt.Sprintf("%x", h.Sum(nil))
 }
--- coreserver/util/hash_test.go
+++ coreserver/util/hash_test.go
@@ -20,3 +20,17 @@ func TestMD5sum(t *testing.T) {
 		assert.Equal(t, c.out, got)
 	}
 }
+
+func TestSHA256(t *testing.T) {
+	var cases = []struct {
+		in  string
+		out string
+	}{
+		{in: "tom", out: "e1608f75c5d7813f3d4031cb30bfb786507d98137538ff8e128a6ff74e84e643"},
+	}
+
+	for _, c := range cases {
+		got := SHA256(c.in)
+		assert.Equal(t, c.out, got)
+	}
+}
